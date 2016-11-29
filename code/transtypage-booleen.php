d08392ae847fde459c49f480676e37c7dd24a8db
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2016-11-29 16:28:06 +0100

    Correction de code fonctionnel pour php >= 5.5
    - ajout d'une note sur la fonction `boolval()` qui ne fonctionne qu'à partir de php >= 5.5
    - intégration de l'appel à la fonctione `boolval()` dans un bloc conditionnel

--- code/transtypage-booleen.php
+++ code/transtypage-booleen.php
@@ -1,6 +1,8 @@
 <?php
 // transtypage en booléen
 
+// attention : `boolval` n'est disponible qu'à partir de PHP 5.5.0
+
 // première méthode de transtypage
 
 $valeur = true;
@@ -50,48 +52,50 @@ var_dump((bool) $valeur);
 
 // deuxième méthode de transtypage avec une fonction
 
-$valeur = true;
-var_dump(boolval($valeur));
+if (version_compare(PHP_VERSION, '5.5.0', '>=')) {
+    $valeur = true;
+    var_dump(boolval($valeur));
 
-$valeur = 1;
-var_dump(boolval($valeur));
+    $valeur = 1;
+    var_dump(boolval($valeur));
 
-$valeur = 3.14;
-var_dump(boolval($valeur));
+    $valeur = 3.14;
+    var_dump(boolval($valeur));
 
-$valeur = "foo";
-var_dump(boolval($valeur));
+    $valeur = "foo";
+    var_dump(boolval($valeur));
 
-$valeur = 'foo';
-var_dump(boolval($valeur));
+    $valeur = 'foo';
+    var_dump(boolval($valeur));
 
-$valeur = array('foo', 1, 3.14);
-var_dump(boolval($valeur));
+    $valeur = array('foo', 1, 3.14);
+    var_dump(boolval($valeur));
 
-$valeur = false;
-var_dump(boolval($valeur));
+    $valeur = false;
+    var_dump(boolval($valeur));
 
-$valeur = 0;
-var_dump(boolval($valeur));
+    $valeur = 0;
+    var_dump(boolval($valeur));
 
-$valeur = 0.0;
-var_dump(boolval($valeur));
+    $valeur = 0.0;
+    var_dump(boolval($valeur));
 
-$valeur = "";
-var_dump(boolval($valeur));
+    $valeur = "";
+    var_dump(boolval($valeur));
 
-$valeur = '';
-var_dump(boolval($valeur));
+    $valeur = '';
+    var_dump(boolval($valeur));
 
-$valeur = "0";
-var_dump(boolval($valeur));
+    $valeur = "0";
+    var_dump(boolval($valeur));
 
-$valeur = '0';
-var_dump(boolval($valeur));
+    $valeur = '0';
+    var_dump(boolval($valeur));
 
-$valeur = array();
-var_dump(boolval($valeur));
+    $valeur = array();
+    var_dump(boolval($valeur));
 
-$valeur = null;
-var_dump(boolval($valeur));
+    $valeur = null;
+    var_dump(boolval($valeur));
+}
 
